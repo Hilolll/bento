777493227b212109c757154be657eb0c5cd29e1c
Hilol <hilollken@gmail.com>
2024-12-14 00:13:54 +0300

    -d github pages packages

--- src/Content.js
+++ src/Content.js
@@ -6,13 +6,13 @@ export default function Content() {
 
         <div className='body__first-blog'>
             <div className='body__first-part'>
-                <div className='body__artist'> <img src={"./img/flowe-icon.png"} alt='flower-icon'></img> <p>Artist Redefining <br></br> Architecture with  AI-Driven Design</p></div>
-                <div className='body__artist__image'><img src={"./img/Portrait.png"} alt='Portrait'></img></div>
+                <div className='body__artist'> <img src={"./img/flowe-icon.png"}></img> <p>Artist Redefining <br></br> Architecture with  AI-Driven Design</p></div>
+                <div className='body__artist__image'><img src={"./img/Portrait.png"}></img></div>
             </div>
             <div className='body__second-part'>
-                <div className='body__second-part__text'><img src={"./img/Vector 2.png"} alt='vector 2'></img><p>Julia Huang is an innovative AI artist, renowned for blending cutting-edge technology with creative expression. Based in LA, she crafts unique digital art experiences accessible globally.</p></div>
-                <a href={'https://t.me/hilllllllol'} className='body__second-part__feedback'><div className='body__second-part__feedback__pointer'>Have some  questions ? 
-                <img src={"./img/Vector.png"} alt='vector'></img></div>Contact Me</a>
+                <div className='body__second-part__text'><img src={"./img/Vector 2.png"}></img><p>Julia Huang is an innovative AI artist, renowned for blending cutting-edge technology with creative expression. Based in LA, she crafts unique digital art experiences accessible globally.</p></div>
+                <a href={'https://t.me/hilllllllol'} className='body__second-part__feedback'><a className='body__second-part__feedback__pointer' href={'https://t.me/hilllllllol'}>Have some  questions ? 
+                <img src={"./img/Vector.png"}></img></a>Contact Me</a>
             </div>
         </div>
 
@@ -21,10 +21,10 @@ export default function Content() {
         <div className='body__second-blog__link'>
 
           <div className='second-blog__link__first-section'><a href={'https://t.me/hilllllllol'} className='musea'>Musea</a>
-          <img src={"./img/Vector.png"} alt='vector'></img> </div>
+          <img src={"./img/Vector.png"}></img> </div>
 
           <div className='second-blog__link__second-section'>
-          <img src={"./img/social-img.png"} alt='social-icon'></img>
+          <img src={"./img/social-img.png"}></img>
           </div>
 
             <p className='second-blog__link__third-section'>Elara</p>
